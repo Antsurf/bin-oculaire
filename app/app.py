94ce08a82de1696b817230367b32299462c996fa
Antoine Richard <antoine.richard972@gmail.com>
2026-06-26 19:28:01 +0200

    Mise au propre du fichier app.py et quelques modifs dans le détail sur les fichiers html. Ajout du carte.html sur lequel il y'aura la carte avec l'agglomération. Dans le app.py, le fichier uploads apparait maintenant dans ap/static/uploads et plus au niveau de la root. Les modifications pour le suivi d'image a été fait dans le fichier gallery.html. La fonction d'upload d'une image a été renommé upload_image au lieu de index et ça a été modifié dans base.html

--- app/app.py
+++ app/app.py
@@ -3,47 +3,116 @@ from werkzeug.utils import secure_filename
 import os
 from markupsafe import escape
 import database as db
+import features as ft
+import uuid
 
-UPLOAD_FOLDER = 'static/uploads'
+UPLOAD_FOLDER = "app/static/uploads/"
+
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 app = Flask(__name__)
+
+# création de la base de donnée en local ou vérification de son existence
 db.init_db()
 
+# création du fichier upload dans lequel toutes les images uploadés via le form sont stockées localement
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 def allowed_file(filename):
+    """
+    Fonction pour vérifier que le fichier donné respecte les extensions mentionnées au dessus
+    """
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# ROUTE VERS L'INDEX (page d'accueil avec le form)
 @app.route('/')
 def base():
     return render_template('index.html')
+
+# ROUTE VERS LA CARTE
+@app.route('/carte')
+def carte():
+    return render_template('carte.html')
+
+# ROUTE VERS LE DASHBOARD
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
+
+# ROUTE VERS LA GALERIE
 @app.route('/gallery')
 def gallery():
     images = db.get_all_images()
     return render_template('gallery.html', images=images)
+
+# ROUTE VERS RESULT 
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+
+
+# Route d'upload d'une image
 @app.route('/index', methods=['GET', 'POST'])
-def index():
+def upload_file():
 
     if request.method == 'POST':
 
         if 'mon_image' not in request.files:
             return redirect(request.url)
+        
         file = request.files['mon_image']
+        
         if file.filename == '':
             return redirect(request.url)
 
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            chemin_final = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
+            # sauvegarde du fichier
+            extension  = file.filename.rsplit('.', 1)[1].lower()
+            filename   = f"{uuid.uuid4().hex}.{extension}"
+            chemin_final = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(chemin_final)
-            db.insert_image(chemin_final)
-            return render_template('index.html')
+
+            id_localisation = None
+            try:
+                img_features = ft.extract_features(chemin_final)
+
+                adresse = request.form.get('mon_adresse', '').strip() #pour éviter les faux espaces
+                # on regarde si on a des coordonnées 
+                """
+                Petite remarque, quand on vérifie que la personne rentre une adresse via 
+                l'api dans index, ça enregistre dans des input cachés la latitude et la longitude
+                cependant, si l'utilisateur écrit simplement une adresse sans en sélectionner une
+                ces valeurs sont vides. Donc on doit utiliser l'appel d'api qu'on a codé 
+                dans features.py 
+                """
+                lat_str = request.form.get('lat')
+                lon_str = request.form.get('lon')
+
+                if lat_str and lon_str:
+                    lat, lon = float(lat_str), float(lon_str)
+                else:
+                    lat, lon = ft.get_coords_from_address(adresse)
+                id_localisation = db.get_localisation(lat, lon)
+
+                # Sinon on la crée
+                if id_localisation is None:
+                    id_localisation = db.add_localisation(lat, lon, adresse)
+
+            except (ValueError, TypeError):
+                # pas de localisation
+                id_localisation = None
+
+            # On insert l'image dans la BDD
+            img_id = db.insert_image(chemin_final, id_localisation)
+            db.add_features(img_id, img_features)
+
+            return redirect(url_for('result'))
+
     return render_template('index.html')
-@app.route('/result')
-def result():
-    return render_template('result.html')
+        
+
+if __name__ == '__main__':
+    print("--- Lancement du serveur sur http://127.0.0.1:5000 ---")
+    app.run(debug=True, port=5000)
